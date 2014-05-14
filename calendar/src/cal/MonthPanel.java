45ae01ed77a02dfd146f7096cac8e605c2d1ea11
bradbricknell <bb136@uni.brighton.ac.uk>
2014-05-14 16:45:57 +0100

    updated monthpanel with appointment detection

--- calendar/src/cal/MonthPanel.java
+++ calendar/src/cal/MonthPanel.java
@@ -7,85 +7,69 @@ import java.awt.GridLayout;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Calendar;
-
-import javax.swing.AbstractButton;
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
- 
-public class MonthPanel extends JPanel implements ActionListener{
+
+public class MonthPanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
 	protected int month;
 	protected int year;
-	
+	protected JButton createApptBtn = new JButton("Create Appointment");
+	protected JButton prevBtn = new JButton("<-");
+	String thisPersonID;
+
 	protected String[] monthNames = { "January", "February", "March", "April",
 			"May", "June", "July", "August", "September", "October",
 			"November", "December" };
 
 	protected String[] dayNames = { "S", "M", "T", "W", "T", "F", "S" };
 
-	private JPanel ctrlPanel;
-
-	private JButton createApptBtn;
-
-	private AbstractButton weeklyViewBtn;
-
-	public MonthPanel(int month, int year) {
+	public MonthPanel(int month, int year, String PersonID) {
 		this.month = month;
 		this.year = year;
+		thisPersonID = PersonID;
 
 		this.add(createGUI());
+
 	}
-	
 
 	protected JPanel createGUI() {
+		createApptBtn.addActionListener(this);
 
-		
-		
 		JPanel monthPanel = new JPanel(true);
 		monthPanel.setLayout(new BorderLayout());
 		monthPanel.setBackground(Color.WHITE);
 		monthPanel.setForeground(Color.BLACK);
 
+		monthPanel.add(createApptBtn);
 		monthPanel.add(createTitleGUI(), BorderLayout.NORTH);
 		monthPanel.add(createDaysGUI(), BorderLayout.SOUTH);
-		
-		
 
 		return monthPanel;
 	}
 
 	protected JPanel createTitleGUI() {
 		JPanel titlePanel = new JPanel(true);
-		titlePanel.setLayout(new GridLayout(2,0));
+		titlePanel.setLayout(new FlowLayout());
 		titlePanel.setBackground(Color.WHITE);
 
 		JLabel label = new JLabel(monthNames[month] + " " + year);
 		label.setForeground(SystemColor.activeCaption);
 
 		titlePanel.add(label, BorderLayout.CENTER);
-		
-		//control buttons panel
-		ctrlPanel = new JPanel(new FlowLayout());
-		createApptBtn = new JButton("Create Appointment");
-		createApptBtn.addActionListener(this);
-		weeklyViewBtn = new JButton("Weekly View");
-		weeklyViewBtn.addActionListener(this);
-		ctrlPanel.add(createApptBtn);
-		ctrlPanel.add(weeklyViewBtn);
-		
-		
-		titlePanel.add(ctrlPanel);
 		return titlePanel;
 	}
 
 	protected JPanel createDaysGUI() {
 		JPanel dayPanel = new JPanel(true);
-		dayPanel.setLayout(new GridLayout(0, dayNames.length)); 
+		 JButton prevBtn = new JButton();		 
+			prevBtn.setText("<-");	
+		dayPanel.setLayout(new GridLayout(0, dayNames.length));
 		dayPanel.setBounds(getVisibleRect());
 		Calendar today = Calendar.getInstance();
 		int tMonth = today.get(Calendar.MONTH);
@@ -113,16 +97,12 @@ public class MonthPanel extends JPanel implements ActionListener{
 
 		int count = 0;
 		int limit = dayNames.length * 6;
-
+		ArrayList<ArrayList<String>> appointments = fetchPush
+				.fetchAppointments(thisPersonID);
+		ArrayList<String> appdates = new ArrayList<String>();
+		appdates.addAll(appointments.get(3));
 		while (iterator.getTimeInMillis() < maximum.getTimeInMillis()) {
 			JButton b = new JButton();
-
-
-//b.setOpaque(false);
-//b.setContentAreaFilled(false);
-//b.setBorderPainted(false);
-
-
 
 			int lMonth = iterator.get(Calendar.MONTH);
 			int lYear = iterator.get(Calendar.YEAR);
@@ -134,10 +114,20 @@ public class MonthPanel extends JPanel implements ActionListener{
 			if ((lMonth == month) && (lYear == year)) {
 				int lDay = iterator.get(Calendar.DAY_OF_MONTH);
 				dayLabel = (Integer.toString(lDay));
-				if ((tMonth == month) && (tYear == year) && (tDay == lDay)) {
-					b.setBackground(Color.ORANGE);
-				} else {
-					b.setBackground(Color.WHITE);
+				for (int x = 0; x < appdates.size(); x++) {
+					String appYear, appMonth, appDay;
+					int appYeari, appMonthi, appDayi;
+					appYear = appointments.get(3).get(x).substring(0, 4);
+					appMonth = appointments.get(3).get(x).substring(5, 7);
+					appDay = appointments.get(3).get(x).substring(8, 10);
+					appYeari = Integer.parseInt(appYear);
+					appMonthi = Integer.parseInt(appMonth);
+					appMonthi = (appMonthi - 1);
+					appDayi = Integer.parseInt(appDay);
+					if ((appMonthi == month) && (appYeari == year)
+							&& (appDayi == lDay)) {
+						b.setBackground(Color.ORANGE);
+					}
 				}
 			} else {
 				dayLabel = (" ");
@@ -161,13 +151,11 @@ public class MonthPanel extends JPanel implements ActionListener{
 		return dayPanel;
 	}
 
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == createApptBtn) { //Create Appointment button clicked..
+		if (e.getSource() == createApptBtn) { // Create Appointment button
+												// clicked..
 			new CreateAppointment();
-		}else if(e.getSource() == weeklyViewBtn){
-			CalendarFrame.setVisibleView(C.WEEKLY_VIEW);
 		}
 	}
 }
